4f33931d72d0da4a1d6095bd2ada4c1626c78aa4
covanalytics <tsink@covingtonky.gov>
2016-12-13 10:09:06 -0500

    Add files via upload

--- clearanceRate.R
+++ clearanceRate.R
@@ -20,7 +20,6 @@ January15$Month <- "January"
 January15$Year <- "2015"
 January15$Date <- "01/31/2015"
 
-
 February15  <-  read.csv(file="February15.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
 February15 <- data.frame(February15, ind=rep(1:4, nrow(February15)/4))
 February15 <- unstack(February15, V1~ind)
@@ -61,7 +60,6 @@ June15$Month <- "June"
 June15$Year <- "2015"
 June15$Date <- "06/30/2015"
 
-
 July15  <-  read.csv(file="July15.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
 July15 <- data.frame(July15, ind=rep(1:4, nrow(July15)/4))
 July15 <- unstack(July15, V1~ind)
@@ -69,7 +67,6 @@ names(July15) <- c("C_rate", "Count", "Charge", "Charge.Code")
 July15$Month <- "July"
 July15$Year <- "2015"
 July15$Date <- "07/31/2015"
-
 
 #Read the contents of all file into a data.frame. Unstacking clearance report from single column.  split to new row at every 4th row.
 August15  <-  read.csv(file="August15.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
@@ -80,8 +77,6 @@ August15$Month <- "August"
 August15$Year <- "2015"
 August15$Date <- "08/31/2015"
 
-
-
 #Read the contents of all file into a data.frame. Unstacking clearance report from single column.  split to new row at every 4th row.
 September15  <-  read.csv(file="September15.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
 September15 <- data.frame(September15, ind=rep(1:4, nrow(September15)/4))
@@ -90,7 +85,6 @@ names(September15) <- c("C_rate", "Count", "Charge", "Charge.Code")
 September15$Month <- "September"
 September15$Year <- "2015"
 September15$Date <- "09/30/2015"
-
 
 #Read the contents of all file into a data.frame. Unstacking clearance report from single column.  split to new row at every 4th row.
 October15  <-  read.csv(file="October15.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
@@ -101,7 +95,6 @@ October15$Month <- "October"
 October15$Year <- "2015"
 October15$Date <- "10/31/2015"
 
-
 #Read the contents of all file into a data.frame. Unstacking clearance report from single column.  split to new row at every 4th row.
 November15  <-  read.csv(file="November15.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
 November15 <- data.frame(November15, ind=rep(1:4, nrow(November15)/4))
@@ -110,7 +103,6 @@ names(November15) <- c("C_rate", "Count", "Charge", "Charge.Code")
 November15$Month <- "November"
 November15$Year <- "2015"
 November15$Date <- "11/30/2015"
-
 
 #Read the contents of all file into a data.frame. Unstacking clearance report from single column.  split to new row at every 4th row.
 December15  <-  read.csv(file="December15.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
@@ -121,7 +113,6 @@ December15$Month <- "December"
 December15$Year <- "2015"
 December15$Date <- "12/31/2015"
 
-
 #Read the contents of all file into a data.frame. Unstacking clearance report from single column.  split to new row at every 4th row.
 January16  <-  read.csv(file="January16.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
 January16 <- data.frame(January16, ind=rep(1:4, nrow(January16)/4))
@@ -131,7 +122,6 @@ January16$Month <- "January"
 January16$Year <- "2016"
 January16$Date <- "01/31/2016"
 
-
 #Read the contents of all file into a data.frame. Unstacking clearance report from single column.  split to new row at every 4th row.
 February16  <-  read.csv(file="February16.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
 February16 <- data.frame(February16, ind=rep(1:4, nrow(February16)/4))
@@ -140,7 +130,6 @@ names(February16) <- c("C_rate", "Count", "Charge", "Charge.Code")
 February16$Month <- "February"
 February16$Year <- "2016"
 February16$Date <- "02/29/2016"
-
 
 #Read the contents of all file into a data.frame. Unstacking clearance report from single column.  split to new row at every 4th row.
 March16  <-  read.csv(file="March16.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
@@ -208,6 +197,7 @@ combined <- do.call("rbind", list(January15, February15, March15, April15, May15
                                   May16, June16, July16, August16, September16))
 
 
+#Change in format of data
 October16  <-  read.csv(file="NEWFormat_October16.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
 October16 <- data.frame(October16, ind=rep(1:2, nrow(October16)/2))
 October16V1 <- unstack(October16, V1~ind)
@@ -222,6 +212,7 @@ October16$Month <- "October"
 October16$Year <- "2016"
 October16$Date <- "10/31/2016"
 
+#Change in format of data
 November16  <-  read.csv(file="NEWFormat_November16.csv", header=FALSE, na.strings = "NA", stringsAsFactors = FALSE)
 November16 <- data.frame(November16, ind=rep(1:2, nrow(November16)/2))
 November16V1 <- unstack(November16, V1~ind)
